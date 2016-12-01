a85eb7d396cd7b3c5b7040bc1104fc7eeded20c1
Marloxo <marloo-_-@hotmail.com>
2016-12-01 19:03:54 +0200

    :sparkles: Link `CleanRootDirectory` with the new UI

--- Tidy Up/Assets/TidyUp/Plugins/Editor/TidyUpCore.cs
+++ Tidy Up/Assets/TidyUp/Plugins/Editor/TidyUpCore.cs
@@ -7,7 +7,7 @@
 // [X] Create My Own Style                  Main Function
 // [X] Import/Export Setting                New Function
 // [X] Reset Setting                        New Function
-// [ ] Link `CleanRootDirectory` with the new UI
+// [X] Link `CleanRootDirectory` with the new UI
 //-----------------------------------------------------------------
 
 using System;
@@ -39,48 +39,90 @@ public class TidyUpCore
 
     internal static void CleanRootDirectory()
     {
+        folderTemplate = LoadSetting(); //populate folderTemplate with data
+
         DirectoryInfo dir = new DirectoryInfo(Application.dataPath);
         FileInfo[] info = dir.GetFiles("*.*", System.IO.SearchOption.TopDirectoryOnly); //get only files in root Directory
+
+        var _scenesFolder = folderTemplate.folderTemplateList.Find(item => item.assetType == AssetType.Scenes);
+        var _animationsFolder = folderTemplate.folderTemplateList.Find(item => item.assetType == AssetType.Animations);
+        var _audioFolder = folderTemplate.folderTemplateList.Find(item => item.assetType == AssetType.Audio);
+        var _fontsFolder = folderTemplate.folderTemplateList.Find(item => item.assetType == AssetType.Fonts);
+        var _materialsFolder = folderTemplate.folderTemplateList.Find(item => item.assetType == AssetType.Materials);
+        var _modelsFolder = folderTemplate.folderTemplateList.Find(item => item.assetType == AssetType.Models);
+        var _texturesFolder = folderTemplate.folderTemplateList.Find(item => item.assetType == AssetType.Textures);
+        var _prefabsFolder = folderTemplate.folderTemplateList.Find(item => item.assetType == AssetType.Prefabs);
+        var _scriptsFolder = folderTemplate.folderTemplateList.Find(item => item.assetType == AssetType.Scripts);
+
         foreach (FileInfo file in info) //Loop over root files
         {
             int extensionPos = file.ToString().IndexOf(".");
-            string extension = file.ToString().Substring(extensionPos);
+            string extension = file.ToString().Substring(extensionPos).ToLower();
 
             if (extension == ".unity" || extension == ".unity.meta")    //if file is Scene
             {
                 AssetDatabase.MoveAsset(
                     "Assets/" + file.Name,
-                    "Assets/" + FolderStructure._Scenes.ToString() + "/" + file.Name);
+                    Path.Combine("Assets" + _scenesFolder.folderPath + _scenesFolder.folderName, file.Name));
             }
             else if (extension == ".anim" || extension == ".anim.meta")   //if file is Animation
             {
                 AssetDatabase.MoveAsset(
                     "Assets/" + file.Name,
-                    "Assets/" + FolderStructure.Animations.ToString() + "/" + file.Name);
+                    Path.Combine("Assets" + _animationsFolder.folderPath + _animationsFolder.folderName, file.Name));
             }
-            else if (extension == ".ttf" || extension == ".ttf.meta")   //if file is Font
+            else if (extension == ".mp3" || extension == ".mp3.meta" || extension == ".ogg" || extension == ".ogg.meta" ||
+             extension == ".wav" || extension == ".wav.meta" || extension == ".mod" || extension == ".mod.meta" ||
+             extension == ".it" || extension == ".it.meta" || extension == ".s3m" || extension == ".s3m.meta" ||
+             extension == ".xm" || extension == ".xm.meta" || extension == ".aiff" || extension == ".aiff.meta" ||
+             extension == ".aif" || extension == ".aif.meta")   //if file is Audio
             {
                 AssetDatabase.MoveAsset(
                     "Assets/" + file.Name,
-                    "Assets/" + FolderStructure.Fonts.ToString() + "/" + file.Name);
+                    Path.Combine("Assets" + _audioFolder.folderPath + _audioFolder.folderName, file.Name));
+            }
+            else if (extension == ".ttf" || extension == ".ttf.meta" || extension == ".otf" || extension == ".otf.meta")   //if file is Font
+            {
+                AssetDatabase.MoveAsset(
+                    "Assets/" + file.Name,
+                    Path.Combine("Assets" + _fontsFolder.folderPath + _fontsFolder.folderName, file.Name));
             }
             else if (extension == ".mat" || extension == ".mat.meta")   //if file is Material
             {
                 AssetDatabase.MoveAsset(
                     "Assets/" + file.Name,
-                    "Assets/" + FolderStructure.Materials.ToString() + "/" + file.Name);
+                    Path.Combine("Assets" + _materialsFolder.folderPath + _materialsFolder.folderName, file.Name));
+            }
+            else if (extension == ".fbx" || extension == ".fbx.meta" || extension == ".dae" || extension == ".dae.meta" ||
+            extension == ".dxf" || extension == ".dxf.meta" || extension == ".obj" || extension == ".obj.meta" ||
+            extension == ".max" || extension == ".max.meta" || extension == ".blend" || extension == ".blend.meta")   //if file is Model
+            {
+                AssetDatabase.MoveAsset(
+                    "Assets/" + file.Name,
+                    Path.Combine("Assets" + _modelsFolder.folderPath + _modelsFolder.folderName, file.Name));
+            }
+            else if (extension == ".bmp" || extension == ".bmp.meta" || extension == ".exr" || extension == ".exr.meta" ||
+            extension == ".gif" || extension == ".gif.meta" || extension == ".hdr" || extension == ".hdr.meta" ||
+            extension == ".iff" || extension == ".iff.meta" || extension == ".jpg" || extension == ".jpg.meta" ||
+            extension == ".pict" || extension == ".pict.meta" || extension == ".png" || extension == ".png.meta" ||
+            extension == ".psd" || extension == ".psd.meta" || extension == ".tga" || extension == ".tga.meta" ||
+            extension == ".tiff" || extension == ".tiff.meta")   //if file is Texture
+            {
+                AssetDatabase.MoveAsset(
+                    "Assets/" + file.Name,
+                    Path.Combine("Assets" + _texturesFolder.folderPath + _texturesFolder.folderName, file.Name));
             }
             else if (extension == ".prefab" || extension == ".prefab.meta")   //if file is Prefab
             {
                 AssetDatabase.MoveAsset(
                     "Assets/" + file.Name,
-                    "Assets/" + FolderStructure.Prefabs.ToString() + "/" + file.Name);
+                    Path.Combine("Assets" + _prefabsFolder.folderPath + _prefabsFolder.folderName, file.Name));
             }
             else if (extension == ".cs" || extension == ".cs.meta")   //if file is Script
             {
                 AssetDatabase.MoveAsset(
                     "Assets/" + file.Name,
-                    "Assets/" + FolderStructure.Scripts.ToString() + "/" + file.Name);
+                    Path.Combine("Assets" + _scriptsFolder.folderPath + _scriptsFolder.folderName, file.Name));
             }
         }
     }
@@ -169,8 +211,22 @@ public class TidyUpCore
         {
             Folder FT = new Folder();
             FT.folderName = item.ToString();
-            FT.folderPath = @"\";
-            FT.assetType = AssetType.Other;
+
+            //Exception for Graphic Content
+            if (item.ToString() == "Materials" || item.ToString() == "Models" || item.ToString() == "Textures")
+                FT.folderPath = @"\Graphics\";
+            else
+                FT.folderPath = @"\";
+
+            try //Match `AssetType` with the default `FolderStructure`
+            { FT.assetType = (AssetType)Enum.Parse(typeof(AssetType), item.ToString()); }
+            catch (System.Exception)
+            {
+                if (item.ToString() == "_Scenes")
+                    FT.assetType = AssetType.Scenes;
+                else
+                    FT.assetType = AssetType.Other;
+            }
 
             folderTemplate.folderTemplateList.Add(FT);
         }
